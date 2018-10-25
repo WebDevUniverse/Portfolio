abd1b02a1aa5547dd855fcc859b324c08c819863
Sharafat Ali <webdevuniverse@gmail.com>
2018-10-25 06:27:31 +0500

    navbar updated

--- Portfolio_x1/assets/js/index.js
+++ Portfolio_x1/assets/js/index.js
@@ -1,22 +1,31 @@
 //-----------------------------------------------Menu Button
 
 var btnMenu = document.querySelector('.btn_menu');
-var menuBars = document.querySelector('.menu_bars');
-var menuClose = document.querySelector('.menu_close');
+var bar1 = document.querySelector('.bars:nth-child(1)');
+var bar2 = document.querySelector('.bars:nth-child(2)');
+var bar3 = document.querySelector('.bars:nth-child(3)');
+var bar4 = document.querySelector('.bars:nth-child(4)');
 
-var o = 0;
+var bars = 0;
 btnMenu.addEventListener('click', function(){
-    if(o == 0){
-        // btnMenu.setAttribute('style','border: 2px double red');
-        menuBars.setAttribute('style', 'display: none;')
-        menuClose.setAttribute('style', 'display: block;')
-        o = 1;
+
+    if(bars == 0){
+
+        bar1.setAttribute('class', 'barss');
+        bar2.setAttribute('class', 'barss');
+        bar3.setAttribute('class', 'barss');
+        bar4.setAttribute('class', 'barss');
+
+        bars = 1;
     }
     else {
-        // btnMenu.setAttribute('style','border: 2px double green');
-        menuBars.setAttribute('style', 'display: block;')
-        menuClose.setAttribute('style', 'display: none;')
-        o = 0;
+
+        bar1.setAttribute('class', 'bars');
+        bar2.setAttribute('class', 'bars');
+        bar3.setAttribute('class', 'bars');
+        bar4.setAttribute('class', 'bars');
+
+        bars = 0;
     }
 })
 //-----------------------------------------------Menu Button
